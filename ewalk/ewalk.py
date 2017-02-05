13a9aa6390d6a30c553e220ef94bd70ae77a8906
Patrick Spencer <pkspenc@gmail.com>
2017-02-04 22:12:27 -0500

    Add basic api request functionality

--- ewalk/ewalk.py
+++ ewalk/ewalk.py
@@ -1,1 +1,21 @@
 # -*- coding: utf-8 -*-
+
+import requests
+import settings
+import pprint
+
+url = 'http://svcs.ebay.com/services/search/FindingService/v1'
+url += "?OPERATION-NAME=findItemsByKeywords"
+url += "&SERVICE-VERSION=1.0.0"
+url += "&SECURITY-APPNAME="
+url += settings.APP_ID
+url += "&GLOBAL-ID=EBAY-US"
+url += "&RESPONSE-DATA-FORMAT=JSON"
+url += "&callback=_cb_findItemsByKeywords"
+url += "&REST-PAYLOAD"
+url += "&keywords=harry%20potter"
+url += "&paginationInput.entriesPerPage=3"
+r = requests.post(url)
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(r.text)
