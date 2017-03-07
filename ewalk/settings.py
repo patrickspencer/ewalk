2f0cd6e4310a40664a17335a35466de24031b183
Patrick Spencer <pkspenc@gmail.com>
2017-03-07 18:24:23 -0500

    Add settings

---
+++ ewalk/settings.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+    models
+    ~~~~~~
+    Model definitions and class methods
+"""
+
+import os
+import configparser
+
+config = configparser.RawConfigParser()
+config.read('/etc/okapi_production_settings.ini')
+DB_USER = config.get('database', 'DATABASE_USER')
+DB_PASS = config.get('database', 'DATABASE_PASSWORD')
+DB_NAME = config.get('database', 'DATABASE_NAME')
+DB_HOST = config.get('database', 'DATABASE_HOST')
+DB_PORT = config.get('database', 'DATABASE_PORT')
+# DB_URI = 'postgres://%s:%s@%s:%s/%s' % (
+#          DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
+
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+DB_URI = "sqlite:///%s" % os.path.join(BASE_PATH,'db.sqlite3')
+
+LOCAL_TIMEZONE = 'US/Eastern'
+LOGS_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)),'logs')
+
