9dcacd6c56a4bea63a6c1d338dedcc5d8e158b54
Patrick Spencer <pkspenc@gmail.com>
2017-03-12 17:17:11 -0400

    Update test script

--- test.py
+++ test.py
@@ -3,6 +3,6 @@
 from ewalk.models import dbsession
 from ewalk import create_quote
 from yahoo_finance import Share
-from ewalk.models import Quote, Symbol, dbsession
+from stockwalk.models import Quote, Company, dbsession
 
-print(dbsession.query(Symbol).filter(Symbol.name == 'YHOO').first().id)
+print(dbsession.query(Company).filter(Symbol.name == 'YHOO').first().id)
