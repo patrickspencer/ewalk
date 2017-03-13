2a90b349f42f803c9a052c79a0269c32670f0455
Patrick Spencer <pkspenc@gmail.com>
2017-03-12 20:15:53 -0400

    Update graph making py r script

--- stockwalk/populate/make_graphs.py
+++ stockwalk/populate/make_graphs.py
@@ -1,7 +1,19 @@
 from subprocess import call
-# call(["ls", "-l"])
-from os.path import abspath, dirname, join
+from stockwalk.models import dbsession, Company
+from os.path import abspath, dirname, join, expanduser, isfile
 
-data_dir = abspath(join(dirname(__file__), '..', '..',))
-rfile = join(data_dir, 'rfbp.R')
-print(rfile)
+# lib_dir is where rfbp.R is located
+lib_dir = abspath(join(dirname(__file__), '..', '..', 'lib'))
+rfbp_loc = join(lib_dir, 'rfbp.R')
+
+attribute = "close"
+
+graph_dir = join(expanduser("~"), "stockwalk_graphs")
+
+symbols = dbsession.query(Company.symbol).all()
+for s in symbols:
+    symbol = s[0]
+    file_name = symbol + "_" + attribute + ".png"
+    file_full = join(graph_dir, file_name)
+    if not isfile(file_full):
+        call(["Rscript", rfbp_loc, symbol, attribute])
